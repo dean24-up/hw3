208037208b70277b3c728c93243ded5778ba17d0
Sydney Dean <dean24@up.edu>
2025-02-04 15:01:17 -0800

    part 7 complete

---
+++ hw3/public/javascripts/sum.js
@@ -0,0 +1,5 @@
+//Sydney Dean
+function sum(a,b){
+    return a+b;
+}
+module.exports = sum;
---
+++ hw3/tests/sum.test.js
@@ -0,0 +1,10 @@
+/**
+ * Sydney Dean
+ * @jest-environment jsdom
+ */
+
+const sum = require('../public/javascripts/sum.js');
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1,2)).toBe(3);
+    })
+
